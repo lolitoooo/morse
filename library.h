aee39a497c27d4b7bdaa5fd0479b2a601965d894
pena <lpena@myges.fr>
2023-01-26 10:39:21 +0100

    add number

--- library.h
+++ library.h
@@ -2,7 +2,7 @@
 #define LIBRARY_H_INCLUDED
 
 void inputString(char *string, int size);
-void encode(char string[], char tab[26][2][5]);
-void decode(char morse[], char tab[26][2][5]) ;
+void encode(char string[], char tab[36][2][6]);
+void decode(char morse[], char tab[36][2][6]) ;
 
 #endif // LIBRARY_H_INCLUDED
