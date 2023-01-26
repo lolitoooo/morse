2008c2213a0932f99390ec171e167476f2b3ed24
pena <lpena@myges.fr>
2023-01-26 17:51:05 +0100

    add special char

--- library.c
+++ library.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<string.h>
 
-void inputString(char *string, int size){
+void inputString(char *string, int size) {
     fgets(string, size, stdin);
     if(string[strlen(string) - 1] == '\n')
         string[strlen(string) - 1] = '\0';
@@ -9,12 +9,12 @@ void inputString(char *string, int size){
         fflush(stdin);
 }
 
-void encode(char string[], char tab[36][2][6]){
+void encode(char string[], char tab[54][2][11]) {
     int i;
     int j;
     char morse[255] = "";
-    for (i = 0; i < strlen(string); i++){
-        for (j = 0; j < 36; j++){
+    for (i = 0; i < strlen(string); i++) {
+        for (j = 0; j < 54; j++){
             if(strchr(tab[j][0], string[i]) != NULL) {
                 strcat(morse, tab[j][1]);
                 strcat(morse, " ");
@@ -24,10 +24,10 @@ void encode(char string[], char tab[36][2][6]){
     printf("%s\n", morse);
 }
 
-void decode(char morse[], char tab[36][2][6]) {
+void decode(char morse[], char tab[54][2][11]) {
     char *letter = strtok(morse, " ");
     while (letter != NULL) {
-        for (int i = 0; i < 36; i++) {
+        for (int i = 0; i < 54; i++) {
             if (strcmp(letter, tab[i][1]) == 0) {
                 printf("%c", tab[i][0][0]);
             }
